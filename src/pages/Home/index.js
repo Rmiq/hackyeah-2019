23c2f4cb685d7848abd1b83dc833c25cd28a0e46
Remigiusz Chiluta <remigiusz.x.chiluta@gsk.com>
2019-09-14 17:47:37 +0200

    quiz with suggested results

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,21 +4,46 @@ import "./index.scss";
 
 const quizQuestions = [
 	{
-		question: "2nd questions",
-		answers: ["1", "2", "3"]
+		question: "What is the purpose of your flight?",
+		answers: ["Business trip", "City break", "Holidays"]
 	},
 	{
 		question: "2nd questions",
 		answers: ["1", "2", "3"]
 	},
 	{
-		question: "2nd questions",
+		question: "3rd questions",
 		answers: ["1", "2", "3"]
 	}
 ];
 
+const profile = [];
+
 const Home = () => {
-	const { quizCounter, setQuizCounter } = useState(0);
+	const [quizCounter, setQuizCounter] = useState(0);
+	const [quizShow, setQuizShow] = useState(true);
+
+	const quizAnswer = e => {
+        console.log(quizCounter,quizQuestions.length - 1);
+		if (
+			e.target.classList.contains("ans") &&
+			quizCounter < quizQuestions.length -1
+		) {
+			profile.push(e.target.text);
+		} else if (
+            e.target.classList.contains("ans") &&
+			quizCounter == quizQuestions.length -1
+        ){
+            setQuizShow(false);
+            profile.push(e.target.text);
+            
+            // Make post here
+            console.log(profile);
+		}
+		if (quizCounter < quizQuestions.length - 1) {
+			setQuizCounter(quizCounter + 1);
+		}
+	};
 
 	return (
 		<div className="view view-home">
@@ -26,52 +51,63 @@ const Home = () => {
 				<span>
 					Answer those questions to help us find you a destination
 				</span>
-				<h2>What is the purpose of your flight?</h2>
+				<h2>{quizQuestions[quizCounter].question}</h2>
 				<div className="answers">
-					<ul>
+					<ul onClick={e => quizAnswer(e)}>
 						<li>
-							<a>Business Trip</a>
+							<a id="ans-1" class="ans">
+								{quizQuestions[quizCounter].answers[0]}
+							</a>
 						</li>
 						<li>
-							<a>City break</a>
+							<a id="ans-2" class="ans">
+								{quizQuestions[quizCounter].answers[1]}
+							</a>
 						</li>
 						<li>
-							<a>Holidays</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-			<div className="results">
-				<h3>Top results:</h3>
-				<div className="results-list">
-					<ul>
-						<li>
-							<div>
-								<img src="./MIA.jpg" />
-								<span>Miami</span>
-							</div>
-
-							<button>Check prices</button>
-						</li>
-						<li>
-							<div>
-								<img src="./LAX.jpg" />
-								<span>Los Angeles</span>
-							</div>
-
-							<button>Check prices</button>
-						</li>
-						<li>
-							<div>
-								<img src="./JFK.jpg" />
-								<span>New York</span>
-							</div>
-
-							<button>Check prices</button>
+							<a id="ans-3" class="ans">
+								{quizQuestions[quizCounter].answers[2]}
+							</a>
 						</li>
 					</ul>
 				</div>
 			</div>
+			{quizCounter != 0 ? (
+				<div className="results">
+					<h3>Top results:</h3>
+					<div className="results-list">
+						<ul>
+							<li>
+								<div>
+									<img src="./MIA.jpg" />
+									<span>Miami</span>
+								</div>
+
+								<button>Check prices</button>
+							</li>
+							<li>
+								<div>
+									<img src="./LAX.jpg" />
+									<span>Los Angeles</span>
+								</div>
+
+								<button>Check prices</button>
+							</li>
+							<li>
+								<div>
+									<img src="./JFK.jpg" />
+									<span>New York</span>
+								</div>
+
+								<button>Check prices</button>
+							</li>
+						</ul>
+					</div>
+				</div>
+			) : (
+				""
+			)}
+
 			<div className="standard-search">
 				<span>or use a </span>
 				<a>standard search</a>

e786164d3977674e69a17d2de333f5dd018ec822
Remigiusz Chiluta <remigiusz.x.chiluta@gsk.com>
2019-09-14 09:48:07 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">Header</header>
+			<main className="App-main">Main content</main>
+			<footer className="App-footer">Footer</footer>
+		</div>
+	);
+}
+
+export default App;
